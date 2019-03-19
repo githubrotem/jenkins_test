26df136eeff293ef6d3a928f3e551b21f55fff70
githubrotem <48711371+githubrotem@users.noreply.github.com>
2019-03-19 13:31:07 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,25 +1,7 @@
-/******************************************************************************
- *  Compilation:  javac HelloWorld.java
- *  Execution:    java HelloWorld
- *
- *  Prints "Now Its 11:14:56");
- *
- *  % java HelloWorld
- *  Now Its 11:14:56");
- *
- *  These 17 lines of text are comments. They are not part of the program;
- *  they serve to remind us about its properties. The first two lines tell
- *  us what to type to compile and test the program. The next line describes
- *  the purpose of the program. The next few lines give a sample execution
- *  of the program and the resulting output. We will always include such 
- *  lines in our programs and encourage you to do the same.
- *
- ******************************************************************************/
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Now Its 11:14:56");
         System.out.println("");
         System.out.println("Now Its 13:25");
         System.out.println("Rotem is the man!!!");
